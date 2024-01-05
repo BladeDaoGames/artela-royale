21cd931c13f20e346cae420cd23a0fec358f394f
0xlonescv <0xbuilderham@gmail.com>
2024-01-05 14:32:56 +0800

    :sparkles: Add main starter configs

---
+++ client/src/config/chainConfig.ts
@@ -0,0 +1,28 @@
+import { foundry} from "@wagmi/chains";
+import { artela_testnet } from './supportedChains';
+//
+import RRoyale from './abis/RRoyale.json';
+
+export const chainConfigs = {
+    31337 : {
+        config: foundry,
+        contracts: {
+            royale:"0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+            multicall: "0xca11bde05977b3631167028862be2a173976ca11"
+        }
+    },
+    11820: {
+        config: artela_testnet,
+        contracts: {
+            royale:"0x6951354310E37Bd0E5B3e8f18DD883F186042835",
+            multicall: "0xca11bde05977b3631167028862be2a173976ca11"
+        }
+    },
+}
+
+export const chainConfig = {
+    chaindetails: chainConfigs[import.meta.env.VITE_CHAIN_ID]?.config??foundry,
+    royaleContractAddress: chainConfigs[import.meta.env.VITE_CHAIN_ID]?.contracts?.royale,
+    //royaleAbi: Royale.abi
+    royaleAbi: RRoyale.abi
+}
---
+++ client/src/config/supportedChains.ts
@@ -0,0 +1,32 @@
+//import { MUDChain, latticeTestnet } from "@latticexyz/common/chains";
+import { foundry, Chain } from "@wagmi/chains";
+
+export const artela_testnet = {
+    id: 11820,
+    name: 'Artlea Testnet',
+    network: 'artela_testnet',
+    nativeCurrency: {
+        decimals: 18,
+        name: 'Artela',
+        symbol: 'ART',
+    },
+    rpcUrls: {
+      default: {
+        http: ['https://betanet-rpc1.artela.network'],
+        webSocket: ['wss://betanet-rpc1.artela.network'],
+      },
+      public: {
+        http: ['https://betanet-rpc1.artela.network'],
+        webSocket: ['wss://betanet-rpc1.artela.network'],
+      },
+    },
+
+    blockExplorers: {
+        default: { name: 'Artela Scan', url: 'https://betanet-scan.artela.network/' },
+    },
+
+} as const satisfies Chain
+
+// If you are deploying to chains other than anvil or Lattice testnet, add them here
+export const supportedChains: Chain[] = [artela_testnet, foundry];
+//export const supportedChains: MUDChain[] = [bladedao, foundry, latticeTestnet];
