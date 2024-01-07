000222abb275711c6d25b370771d81bdb523bee4
0xlonescv <0xbuilderham@gmail.com>
2024-01-07 10:59:49 +0800

    :green_heart: Update with correct new artela chain id and also add netlify ci

--- client/src/config/chainConfig.ts
+++ client/src/config/chainConfig.ts
@@ -5,17 +5,17 @@ import { artela_testnet } from './supportedChains';
 import RRoyale from './abis/Royale.json';
 
 export const chainConfigs = {
+    11822: {
+        config: artela_testnet,
+        contracts: {
+            royale:"0x0A2A6892BbEc820631527Ea24D7FD22e19fC5E77",
+            multicall: "0xca11bde05977b3631167028862be2a173976ca11"
+        }
+    },
     31337 : {
         config: foundry,
         contracts: {
             royale:"0x5FbDB2315678afecb367f032d93F642f64180aa3",
-            multicall: "0xca11bde05977b3631167028862be2a173976ca11"
-        }
-    },
-    11820: {
-        config: artela_testnet,
-        contracts: {
-            royale:"0x0A2A6892BbEc820631527Ea24D7FD22e19fC5E77",
             multicall: "0xca11bde05977b3631167028862be2a173976ca11"
         }
     },
